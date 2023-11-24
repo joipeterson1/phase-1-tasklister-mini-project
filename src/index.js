698f2bbbb1ea878435b583febf03cfe9ba5a339f
Dalecia <petersondalecia@gmail.com>
2023-11-24 18:23:12 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    buildList(e.target.elements['new-task-description'].value)
+    form.reset()
+  })
+})
+
+function buildList(list){
+let p = document.createElement('p')
+p.textContent = list
+}
+
+function buildList(list){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${list} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e){
+e.target.parentNode.remove()
+}
